806320828168b3f77bde84dfa4feae5ec3c567cc
jnunes-wtf <58940794+jnunes-wtf@users.noreply.github.com>
2020-07-31 23:29:40 -0300

    Imersão React - Aula 2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CadastroVideo from './pages/cadastro/video/index';
+
+import { BrowserRouter,Switch, Route } from 'react-router-dom';
+
+const Erro404 = () => (<div>Erro 404 - Not Found</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+
+
+    </Switch>
+    
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
